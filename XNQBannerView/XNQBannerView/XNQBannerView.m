3d519ab960781f161efb8614dd2b93250a7911ad
QFPayShadowMan <qfpayshadowman@pn-206-196.itsc.cuhk.edu.hk>
2016-02-19 13:55:34 +0800

    fix drag
    手势拖动和自动滚动不冲突，增加nstimer的停止和开启

--- XNQBannerView/XNQBannerView/XNQBannerView.m
+++ XNQBannerView/XNQBannerView/XNQBannerView.m
@@ -12,7 +12,7 @@
 #define kBannerWidth self.bounds.size.width
 #define kBannerHeight self.bounds.size.height
 
-static NSString *const strBannerDefaultImageName = @"1.jpg";
+static NSString *const strBannerDefaultImageName = @"default";
 
 @interface XNQBannerView () <
 UIGestureRecognizerDelegate,
@@ -63,7 +63,7 @@ UIScrollViewDelegate > {
     self.showsVerticalScrollIndicator = NO;
     self.pagingEnabled = YES;
     self.contentSize = CGSizeMake(kBannerWidth * 3, kBannerHeight);
-    self.contentOffset = CGPointMake(kBannerWidth, 0);
+    self.contentOffset = CGPointMake(kBannerWidth, 0);//** 默认在中间位置
     _imageIndex = 0;
     _leftImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, kBannerWidth, kBannerHeight)];
     _centerImageView = [[UIImageView alloc]initWithFrame:CGRectMake(kBannerWidth, 0, kBannerWidth, kBannerHeight)];
@@ -85,25 +85,25 @@ UIScrollViewDelegate > {
     //[_pageControl addTarget:self action:@selector(pageControlValueChange) forControlEvents:UIControlEventValueChanged];
 }
 
-/*   
- // ** 对pageControl的点击处理
-- (void)pageControlValueChange {
-    if (_pageControl.currentPage == 0) {
-        _leftImageView.image = _arrImage[_arrImage.count - 1];
-        _centerImageView.image = _arrImage[0];
-        _rightImageView.image = _arrImage[1];
-    }
-    else if (_pageControl.currentPage == _arrImage.count - 1) {
-        _leftImageView.image = _arrImage[_pageControl.currentPage-1];
-        _centerImageView.image = _arrImage[_pageControl.currentPage];
-        _rightImageView.image = _arrImage[0];
-    }
-    else {
-        _leftImageView.image = _arrImage[_pageControl.currentPage-1];
-        _centerImageView.image = _arrImage[_pageControl.currentPage];
-        _rightImageView.image = _arrImage[_pageControl.currentPage+1];
-    }
-}
+/*
+ // ** 对pageControl的点击处理 需要的可以打开
+ - (void)pageControlValueChange {
+ if (_pageControl.currentPage == 0) {
+ _leftImageView.image = _arrImage[_arrImage.count - 1];
+ _centerImageView.image = _arrImage[0];
+ _rightImageView.image = _arrImage[1];
+ }
+ else if (_pageControl.currentPage == _arrImage.count - 1) {
+ _leftImageView.image = _arrImage[_pageControl.currentPage-1];
+ _centerImageView.image = _arrImage[_pageControl.currentPage];
+ _rightImageView.image = _arrImage[0];
+ }
+ else {
+ _leftImageView.image = _arrImage[_pageControl.currentPage-1];
+ _centerImageView.image = _arrImage[_pageControl.currentPage];
+ _rightImageView.image = _arrImage[_pageControl.currentPage+1];
+ }
+ }
  */
 
 - (void)setupTimer {
@@ -116,8 +116,7 @@ UIScrollViewDelegate > {
 
 - (void)scrollTimer {
     [self setContentOffset:CGPointMake(self.contentOffset.x + kBannerWidth, 0) animated:YES];
-    [self scrollViewDidEndDecelerating:self];
-    _pageControl.currentPage == _arrImage.count - 1 ? _pageControl.currentPage = 0 : _pageControl.currentPage ++;
+    [self performSelector:@selector(scrollViewDidEndDecelerating:) withObject:nil afterDelay:0.4f];
 }
 
 #pragma mark -- set method
@@ -158,24 +157,26 @@ UIScrollViewDelegate > {
                                                                     options:0
                                                                    progress:^(NSInteger receivedSize,
                                                                               NSInteger expectedSize) {
-                        
-                    } completed:^(UIImage *image,
-                                  NSError *error,
-                                  SDImageCacheType cacheType,
-                                  BOOL finished,
-                                  NSURL *imageURL) {
-                        //** 下载完成 更新图片
-                        [_arrImage replaceObjectAtIndex:i withObject:image];
-                        if (_imageIndex == i) {
-                            _centerImageView.image = image;
-                        }
-                        else if (_imageIndex == i-1) {
-                            _rightImageView.image = image;
-                        }
-                        else if (_imageIndex == i+1) {
-                            _leftImageView.image = image;
-                        }
-                    }];
+                                                                       
+                                                                   } completed:^(UIImage *image,
+                                                                                 NSError *error,
+                                                                                 SDImageCacheType cacheType,
+                                                                                 BOOL finished,
+                                                                                 NSURL *imageURL) {
+                                                                       //** 下载完成 更新图片
+                                                                       if (image) {
+                                                                           [_arrImage replaceObjectAtIndex:i withObject:image];
+                                                                           if (_imageIndex == i) {
+                                                                               _centerImageView.image = image;
+                                                                           }
+                                                                           else if (_imageIndex == i-1) {
+                                                                               _rightImageView.image = image;
+                                                                           }
+                                                                           else if (_imageIndex == i+1) {
+                                                                               _leftImageView.image = image;
+                                                                           }
+                                                                       }
+                                                                   }];
                 }
                 else {
                     [marrImage replaceObjectAtIndex:i withObject:[UIImage imageNamed:imageObj]];
@@ -215,18 +216,26 @@ UIScrollViewDelegate > {
     }
 }
 
-#pragma mark - Delegate
-
+#pragma mark - ScrollView Delegate
+// ** 处理滚动效果
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    if (scrollView.contentOffset.x == kBannerWidth) {//** 滚动回第二个图不做处理
-        return;
+    if (!_timer) {// ** 拖动处理
+        if (scrollView.contentOffset.x == kBannerWidth) {//** 滚动回第二个图不做处理
+            return;
+        }
+        if (scrollView.contentOffset.x == 0) {//** 滚动到第一个
+            _imageIndex = _imageIndex == 0 ? _arrImage.count - 1 : _imageIndex - 1;
+        }
+        else if (scrollView.contentOffset.x == kBannerWidth * 2) {//** 滚动到第三个
+            _imageIndex = _imageIndex == _arrImage.count - 1 ? 0 : _imageIndex + 1;
+        }
+        
+        [self setupTimer];
     }
-    if (scrollView.contentOffset.x == 0) {//** 滚动到第一个
-        _imageIndex = _imageIndex == 0 ? _arrImage.count - 1 : _imageIndex - 1;
+    else {// ** 自己滚动处理
+        _imageIndex = _imageIndex = _imageIndex == _arrImage.count - 1 ? 0 : _imageIndex + 1;
     }
-    else if (scrollView.contentOffset.x == kBannerWidth * 2) {//** 滚动到第三个
-        _imageIndex = _imageIndex == _arrImage.count - 1 ? 0 : _imageIndex + 1;
-    }
+    
     if (_imageIndex == 0) {
         _leftImageView.image = _arrImage[_arrImage.count - 1];
         _centerImageView.image = _arrImage[0];
@@ -245,6 +254,13 @@ UIScrollViewDelegate > {
     self.contentOffset = CGPointMake(kBannerWidth, 0);
     _centerImageView.tag = _imageIndex;
     _pageControl.currentPage = _imageIndex;
+}
+
+- (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView {
+    if (_timer) {
+        [_timer invalidate];
+        _timer = nil;
+    }
 }
 
 @end
